a557dc2dfe08b4be3f8e2de69d7f06ba3969ef3d
Jasmin Moffette <Jasmin.moffette@icloud.com>
2025-02-12 16:41:10 -0500

    fonctions des produits

--- produit.py
+++ produit.py
@@ -13,4 +13,20 @@ class Produit:
     def __str__(self):
         return f"Produit : {self.description} - Numéro : {self.code} - Type : {self.type} - Positionnement : {self.po} - Emplacement : {self.emplacement} - Dimension : {self.dimension} - Quantité : {self.quantite}"
     
+    def ajouter_quantite(self, quantite):
+        self.quantite += quantite
+    
+    def retirer_quantite(self, quantite):
+        self.quantite -= quantite
+    
+    def definir_emplacement(self, emplacement):
+        self.emplacement = emplacement
+    
+    def definir_dimension(self, dimension):
+        self.dimension = dimension
+    
+    def definir_po(self, po):
+        self.po = po
+    
+    
 
